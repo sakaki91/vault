4a15d238791d091a6ac36c6f658fe887fea2f7cc
sakaki91 <sakaki@tuta.io>
2026-08-16 19:35:25 -0300

    Update

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-VAULT_DIR=$HOME/.local/share/vault
+VAULT_PATH=$HOME/.local/share/vault
 VAULT_BIN=$HOME/.local/bin
 
-if [[ ! -d $VAULT_DIR || ! -d $VAULT_BIN ]]; then
-    mkdir -p $VAULT_DIR $VAULT_BIN
-	mkdir -p $VAULT_DIR/{config,prefix_tree}
+if [[ $EUID == 0 ]]; then
+    printf "\033[0;93mWARNING!!!\033[0m DO NOT RUN THE INSTALLER AS ROOT.\n"
+else
+    mkdir -p $VAULT_PATH/{config,prefix_tree}
+    mkdir -p $VAULT_BIN
+    /usr/bin/install vault $VAULT_BIN
 fi
-
-/usr/bin/install vault $VAULT_DIR
-ln -sf $VAULT_DIR/vault $VAULT_BIN/vault
-
-printf "* vault installed in: '$VAULT_DIR' \033[0;94m\n --> with a symbolic link in: '$VAULT_BIN'\033[0m\n"
